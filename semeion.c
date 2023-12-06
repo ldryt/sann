7f40c258ce07cfe3c0aa18d6f5ea96e9f756dd91
Lucas Ladreyt <lucas.ladreyt@epita.fr>
2023-12-06 15:54:32 +0100

    semeion: improved testing

--- semeion.c
+++ semeion.c
@@ -1,18 +1,17 @@
 #include "neural_network.h"
 #include "utils/semeion_utils.h"
 
-#define EPOCHS          300
+#define EPOCHS          30
 #define NB_INPUTS       256 // 16x16
 #define NB_HIDDEN       32
 #define NB_OUTPUTS      10
 #define LEARNING_RATE   1.33
 #define LRATE_MODIFIER  0.996
 
-int main()
+network new_network(dataset ds)
 {
-    // srand(time(0));
-    dataset ds = build("./datasets/semeion/semeion.data", NB_INPUTS, NB_OUTPUTS);
     network net = init_network(NB_INPUTS, NB_HIDDEN, NB_OUTPUTS);
+
     double lrate = LEARNING_RATE;
     for (int i = 0; i < EPOCHS; i++)
     {
@@ -30,16 +29,35 @@ int main()
 
         lrate *= LRATE_MODIFIER;
     }
+    printf("\n----\n\n");
+
+    return net;
+}
+
+int main(int argc, char *argv[])
+{
+    if (argc > 2)
+        errx(EXIT_FAILURE, "Only one path can be passed");
+    
+    network net;
+    dataset ds = build("./datasets/semeion/semeion.data", NB_INPUTS, NB_OUTPUTS);
+    if (argc == 1)
+        net = new_network(ds);
+    else
+        net = load_network(argv[1]);
+    
+    shuffle(ds);
 
     double* input = ds.input[0];
     double* target = ds.target[0];
     double* prediction = feed(net, input);
 
-    printf("\n----\n\n");
     printf("Target:\n");
     print_array(target, NB_OUTPUTS);
     printf("Prediction:\n");
     print_array(prediction, NB_OUTPUTS);
+
+    save_network(net, "./semeion.net");
 
     free_dataset(ds);
     free_network(net);
