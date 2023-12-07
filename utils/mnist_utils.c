3102988d580c57f04879b38b44feaa30921aaf32
Lucas Ladreyt <lucas.ladreyt@epita.fr>
2023-12-07 16:02:56 +0100

    mnist: brand new parser

--- utils/mnist_utils.c
+++ utils/mnist_utils.c
@@ -14,9 +14,9 @@ int32_t is_bigendian()
 }
 
 // Swap endianness of an 32 bit integer
-int32_t swap_endianness(int32_t n)
+uint32_t swap_endianness(uint32_t n)
 {
-    char c[4];
+    unsigned char c[4];
 
     if (is_bigendian())
         return n;
@@ -28,48 +28,91 @@ int32_t swap_endianness(int32_t n)
         c[2] = (n >> 16) & 255;
         c[3] = (n >> 24) & 255;
 
-        return ((int32_t)c[0] << 24) + ((int32_t)c[1] << 16)
-               + ((int32_t)c[2] << 8) + c[3];
+        return ((uint32_t)c[0] << 24) + ((uint32_t)c[1] << 16)
+               + ((uint32_t)c[2] << 8) + c[3];
     }
 }
 
-// Parses inputs and targets values from the image number 'set' in
-// the mnist dataset
-void parse_mnist(dataset ds, char *images_path, char *labels_path, size_t set)
+// Build a mnist dataset
+dataset build_mnist(char *images_path, char *labels_path)
 {
-    int32_t header_buffer[0];
-    unsigned char pixels_buffer[28];
+    uint32_t img_buffer[4];
+    uint32_t lbl_buffer[2];
 
     FILE *fp_images = fopen(images_path, "r");
     FILE *fp_labels = fopen(labels_path, "r");
 
+    if (fp_images == NULL || fp_labels == NULL)
+        errx(EXIT_FAILURE, "parse_mnist: error while opening file");
+
     // I could use a buffer of 4 bytes and directly swap each byte, but I
     // wanted to implement the idea behind this nice article:
     // https://developer.ibm.com/articles/au-endianc/
-    fread(header_buffer, 4, 1, fp_images);
-    if (swap_endianness(header_buffer[0]) != 2051)
-        errx(EXIT_FAILURE, "parse_mnist: bad magic number (got 0x%08x)",
-            swap_endianness(header_buffer[0]));
+    fread(img_buffer, sizeof(uint32_t), 4, fp_images);
+    if (swap_endianness(img_buffer[0]) != 2051)
+        errx(EXIT_FAILURE, "bad magic number for images dataset (got 0x%08x)",
+            swap_endianness(img_buffer[0]));
 
-    // random-looking formula to set position to the image number 'set'
-    fseek(fp_images, (4 * 4) + set * (28 * 28), SEEK_SET);
+    fread(lbl_buffer, sizeof(uint32_t), 2, fp_labels);
+    if (swap_endianness(lbl_buffer[0]) != 2049)
+        errx(EXIT_FAILURE, "bad magic number for labels dataset (got 0x%08x)",
+            swap_endianness(lbl_buffer[0]));
 
-    for (size_t i = 0; i < 28; i++)
+    if (img_buffer[1] != lbl_buffer[1])
+        errx(EXIT_FAILURE,
+            "inconsistencies between labels and images datasets");
+
+    size_t nb_sets = swap_endianness(lbl_buffer[1]);
+    size_t nb_inputs
+        = swap_endianness(img_buffer[2]) * swap_endianness(img_buffer[3]);
+    dataset ds = init_dataset(nb_sets, nb_inputs, 10);
+
+    for (size_t set = 0; set < nb_sets; set++)
+        parse_mnist_data(ds, images_path, labels_path, set);
+    
+    fclose(fp_images);
+    fclose(fp_labels);
+
+    return ds;
+}
+
+// Parses inputs and targets values from the image and label number 'set' in
+// the mnist dataset
+void parse_mnist_data(
+    dataset ds, char *images_path, char *labels_path, size_t set)
+{
+    FILE *fp_images = fopen(images_path, "r");
+    FILE *fp_labels = fopen(labels_path, "r");
+    unsigned char c;
+
+    fseek(fp_images, 4 * sizeof(uint32_t) + set * ds.nb_inputs, SEEK_SET);
+    for (size_t i = 0; i < ds.nb_inputs; i++)
     {
-        fread(pixels_buffer, 1, 28, fp_images);
-        for (size_t j = 0; j < 28; j++)
-            ds.input[set][i + j] = pixels_buffer[j];
+        c = fgetc(fp_images);
+        ds.input[set][i] = (double)c;
     }
+
+    fseek(fp_labels, 2 * sizeof(uint32_t) + set, SEEK_SET);
+    c = fgetc(fp_labels);
+    for (size_t k = 0; k < ds.nb_outputs; k++)
+        ds.target[set][k] = (double)(c == k);
 
     fclose(fp_images);
     fclose(fp_labels);
 }
 
+/*
 int main()
 {
-    if (is_bigendian())
-        printf("%d\n", is_bigendian());
-    dataset ds = init_dataset(60000, 28 * 28, 10);
-    parse_mnist(ds, "./datasets/mnist/train-images.idx3-ubyte", "./datasets/mnist/train-labels.idx3-ubyte", 0);
+    char *si = "./datasets/mnist/train-images.idx3-ubyte";
+    char *sl = "./datasets/mnist/train-labels.idx1-ubyte";
+    dataset ds = build_mnist(si, sl);
+    int set = 5999;
+    for (size_t i = 0; i < ds.nb_outputs; i++)
+        printf("%lf\n", ds.target[set][i]);
+    for (size_t i = 0; i < ds.nb_inputs; i++)
+        printf("%03.0lf%s", ds.input[set][i],
+            (i + 1) % 28 == 0 ? "\n" : " ");
     return 0;
 }
+*/
--- utils/mnist_utils.h
+++ utils/mnist_utils.h
@@ -1,18 +1,22 @@
 #ifndef MNIST_UTILS_H
 #define MNIST_UTILS_H
 
-#include <stdio.h>
 #include <stdint.h>
+#include <stdio.h>
 #include "dataset_utils.h"
 
 // Check if system is big-endian
 int is_bigendian();
 
 // Swap endianness of an 32 bit integer
-int32_t swap_endianness(int32_t n);
+uint32_t swap_endianness(uint32_t n);
 
-// Parses inputs and targets values from the image number 'set' in
+// Build a mnist dataset
+dataset build_mnist(char *images_path, char *labels_path);
+
+// Parses inputs and targets values from the image and label number 'set' in
 // the mnist dataset
-void parse_mnist(dataset ds, char *images_path, char *labels_path, size_t set);
+void parse_mnist_data(
+    dataset ds, char *images_path, char *labels_path, size_t set);
 
 #endif
